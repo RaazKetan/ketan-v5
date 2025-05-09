3233f332ac5d19999637ddfbb565f1e04263c32b
ketanraaz <ketanraaz@google.com>
2025-05-09 14:51:40 +0530

    Home Screen Bug

---
+++ src/components/pages/home/info/info.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+export const Info = () => {
+    return (
+        <div className="flex items-center justify-center">
+        <div className="text-center">
+            <h1 className="text-4xl font-bold">Ketan Raj </h1>
+            <h2>(SDE)</h2>
+            <h3>@Google</h3>
+        </div>
+        </div>
+    );
+}
