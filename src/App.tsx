6d91e5a86ca2fc5276b09cc649a579ad67c8b917
ketanraaz <ketanraaz@google.com>
2025-05-09 09:51:27 +0530

    Fixed loader Screen Bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/home/home';
+import Home from './components/pages/home/home';
 
 const App: React.FC = () => {
   return (
--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 
-export const Loader: React.FC<{ onFinish: () => void }> = ({ onFinish }) => {
+export const Loader = () => {
   const loaderRef = useRef(null);
   const textRef = useRef(null);
-  const progressBarRef = useRef(null);
   const [count, setCount] = useState(0);
   const [position, setPosition] = useState({ x: 0, y: 0 });
-
 
   // GSAP animation to smoothly move the cursor
   useEffect(() => {
@@ -15,12 +13,12 @@ export const Loader: React.FC<{ onFinish: () => void }> = ({ onFinish }) => {
       setPosition({ x: event.clientX, y: event.clientY });
     };
 
-    window.addEventListener("mousemove", handleMouseMove); 
+    window.addEventListener("mousemove", handleMouseMove);
 
     gsap.to(textRef.current, {
       x: position.x,
       y: position.y,
-      duration: 0.2, 
+      duration: 0.2,
       overwrite: true,
       ease: "power3.out",
     });
@@ -31,32 +29,27 @@ export const Loader: React.FC<{ onFinish: () => void }> = ({ onFinish }) => {
 
   // GSAP animation to fade out the loader
   useEffect(() => {
-    const tl = gsap.timeline({
-      onComplete: () => {
-        onFinish();
-      },
-    });
+    const tl = gsap.timeline({});
 
     tl.fromTo(
       textRef.current,
-      { opacity: 0, scale: 0.8 },
-      { opacity: 1, scale: 1, duration: 1, ease: "power2.out" }
+      { y: 100 },
+      { y: 0, opacity: 1, duration: 1, ease: "power4.out" }
     ).to(loaderRef.current, {
-      opacity: 1,
-      duration: 1,
-      ease: "power2.out",
-      onComplete: () => {
-        gsap.set(loaderRef.current, { display: "none" });
-      },
+      y: "-100%",
+      duration: 1.5,
+      ease: "power2.inOut",
+      delay: 3,
+      opacity: 0,
     });
-  }, [onFinish]);
-  
+  }, []);
+
   //percentage loader
   useEffect(() => {
-    if(count < 100){
+    if (count < 100) {
       const intervalId = setInterval(() => {
         setCount((prevCount) => prevCount + 1);
-      }, 30);
+      }, 40);
 
       return () => clearInterval(intervalId);
     }
@@ -64,25 +57,20 @@ export const Loader: React.FC<{ onFinish: () => void }> = ({ onFinish }) => {
 
   // cursor disappears when loading is done
   useEffect(() => {
-    document.body.classList.add('loading');
+    document.body.classList.add("loading");
     return () => {
-      document.body.classList.remove('loading');
+      document.body.classList.remove("loading");
     };
   }, []);
   return (
-
-    <div
-      ref={loaderRef}
-      className="w-full h-full bg-amber-300"
-    >
+    <div ref={loaderRef} className="w-full h-full fixed top-0 left-0">
       {/* Your other content goes here */}{" "}
       <div
         ref={textRef}
-        className="fixed top-0 left-0 pointer-none: text-lg  translate-x-[-50%] translate-y-[-50%]"
+        className="fixed top-0 left-0 pointer-none: text-lg  translate-x-[-50%] translate-y-[-50%] text-[#333]"
       >
-          Loading {count}%
-      </div>
-      {" "}
+        Loading {count}%
+      </div>{" "}
     </div>
   );
 };
